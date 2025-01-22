14f6b2e1297e276d84ae6af0e7b77f43a5daafb0
Matheus Viana de Oliveira <matheus.viana@hotmail.com>
2025-01-22 15:56:06 -0300

    GET game by id

--- src/main/java/com/devmath/dslist/controllers/GameController.java
+++ src/main/java/com/devmath/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devmath.dslist.controllers;
 
+import com.devmath.dslist.dto.GameDTO;
 import com.devmath.dslist.dto.GameMinDTO;
 import com.devmath.dslist.entities.Game;
 import com.devmath.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 
 }
--- src/main/java/com/devmath/dslist/services/GameService.java
+++ src/main/java/com/devmath/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devmath.dslist.services;
 
+import com.devmath.dslist.dto.GameDTO;
 import com.devmath.dslist.dto.GameMinDTO;
 import com.devmath.dslist.entities.Game;
 import com.devmath.dslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class GameService {
     @Autowired // dependency injection
     private GameRepository gameRepository;
 
+    @Transactional
     public List<GameMinDTO> findAll() {
         List<Game> games = gameRepository.findAll();
         List<GameMinDTO> dto = games.stream().map(GameMinDTO::new).toList();
@@ -21,5 +24,12 @@ public class GameService {
         return dto;
     }
 
+    @Transactional // ensures that the transaction with the DB obey the ACID principles
+    public GameDTO findById(Long id) {
+        // findById returns an optional, .get() to get the information of this return
+        Game game = gameRepository.findById(id).get();
+        // GameDTO gameDTO = new GameDTO(game);
+        return new GameDTO(game);
+    }
 
 }
